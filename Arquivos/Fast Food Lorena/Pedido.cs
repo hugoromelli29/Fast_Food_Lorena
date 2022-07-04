7bf93693123cf3f842ce6b76484b1004bc0992cd
hugoromelli29 <hugoromelli@hotmail.com>
2022-07-04 09:22:59 -0300

    Upload arquivos

---
+++ Arquivos/Fast Food Lorena/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fastfood
+{
+    public class Pedido
+    {
+        public Cliente Cliente { get; set; }
+        public string Codigo { get; set; }
+        public double Valor { get; set; }
+        public string Itens { get; set; }
+        public void ExibirPedido()
+        {
+            Console.WriteLine("O código do pedido é " + Codigo);
+            Console.WriteLine("O nome do cliente é " + Cliente.Nome);
+            Console.WriteLine("O endereço do cliente é " + Cliente.Endereco);
+            Console.WriteLine("O valor é " + Valor);
+            Console.WriteLine("Os itens são " + Itens);
+            Console.WriteLine();
+            Console.WriteLine("Tecle Enter para continuar...");
+        }
+        public Pedido(string codigo)
+        {
+            Codigo = codigo;
+        }
+    }
+}
---
+++ Arquivos/Fast Food Lorena/Program.cs
@@ -0,0 +1,38 @@
+﻿using fastfood;
+
+Cliente cliente1 = new();
+cliente1.Nome = "Rodrigo Santos";
+cliente1.Endereco = "R. Palmares, 175";
+
+Pedido lanche1 = new("001");
+lanche1.Cliente = cliente1;
+lanche1.Valor = 15.90;
+lanche1.Itens = "Cheddar, Bacon, Salada";
+
+Pedido lanche2 = new("002");
+lanche2.Cliente = cliente1;
+lanche2.Valor = 15.90;
+lanche2.Itens = "Ovo, Bacon, Salada";
+
+Begin: Console.WriteLine("Pedidos pendentes: 001 / 002");
+Console.WriteLine();
+Console.WriteLine("Digite o código do pedido para visualizá-lo: ");
+string codigoDigitado = Console.ReadLine();
+if (codigoDigitado == "001")
+    lanche1.ExibirPedido();
+else if (codigoDigitado == "002")
+    lanche2.ExibirPedido();
+else
+{
+    Console.WriteLine("Pedido não encontrado.");
+    Console.WriteLine("Tecle Enter para continuar...");
+}
+
+Console.ReadKey();
+Console.WriteLine("Para voltar ao início, tecle [s] e Enter. Para sair, tecle outra letra e Enter...");
+char entrada = char.Parse(Console.ReadLine());
+if (entrada == 's' || entrada == 'S')
+{
+    Console.Clear();
+    goto Begin;
+}
